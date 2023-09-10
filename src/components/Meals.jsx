62934c720ef90f408022d9cf4251784137ea70a6
danieldegirmenci <115356280+danieldegirmenci@users.noreply.github.com>
2023-09-10 15:49:32 +0000

    edited meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,27 +1,33 @@
 import { useGlobalContext } from "../context.jsx"
+import { GrFavorite } from 'react-icons/gr';
+
 const Meals = () => {
 
   const {meals} = useGlobalContext();
 
   return (
-<section class="m-4">
-  <div className="container" >
-    <div class="row d-flex justify-content-center">
+<section class="m-3">
+  <div className="container-sm" >
+    <div className="row d-flex justify-content-center">
   
   {
     meals.map((meal)=>{
       console.log(meal);
       
-      return <div className="card m-2 col-md-3 " style={{"min-width":"6rem"}} >
+      return <div className="card m-2 col-6 col-sm-3 col-md-3 col-lg-3 text-center " key={meal.idMeal}  >
         <img src={meal.strMealThumb} className="card-img-top" alt="..."/>
                <div className="card-body">
             <h5 className="card-title">{meal.strMeal}</h5>
-     <ul class="list-group list-group-flush">
-    <li class="list-group-item">{meal.strArea}</li>
-    <li class="list-group-item text-truncate">{meal.strCategory}</li>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item text-truncate">{meal.strArea}</li>
+                <li className="list-group-item text-truncate">{meal.strCategory}</li>
+                <li className="list-group-item text-truncate">  
+               <GrFavorite style={{color:"red"}}/></li>
+               
     
        </ul>
-             <button  className="btn btn-danger">Recipee</button>
+             <button type="button"  className=" btn btn-danger mt-2">Recipee</button>
+
                 </div>
        
       </div>
